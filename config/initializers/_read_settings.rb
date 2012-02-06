d9c73f7438c199648e230114e800ac02afd7aac4
Evgeny Lapin <lev@openteam.ru>
2012-02-06 12:38:26 +0700

    remove unused stuff

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -7,11 +7,6 @@ Settings.define 'hoptoad.api_key',            :env_var => 'HOPTOAD_API_KEY'
 Settings.define 'hoptoad.host',               :env_var => 'HOPTOAD_HOST'
 
 Settings.define 'cms.url',                    :env_var => 'CMS_URL',          :required => true
-
-Settings.define 'cms.protocol',               :env_var => 'CMS_PROTOCOL',     :default => :http
-Settings.define 'cms.host',                   :env_var => 'CMS_HOST',         :required => true
-Settings.define 'cms.port',                   :env_var => 'CMS_PORT',         :required => true
-
 Settings.define 'cms.site_slug',              :env_var => 'CMS_SITE_SLUG',    :required => true
 
 Settings.resolve!
