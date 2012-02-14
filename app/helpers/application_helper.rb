416da94db661b280b542b9f09144fc73ae6f76e8
Evgeny Lapin <lev@openteam.ru>
2012-02-14 16:06:43 +0700

    rf styles: remove unstuff. show news with real info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def news_image(url, alt = '')
+    width, height = url.match(%r{files/\d+/(\d+)-(\d+)})[1..2]
+    image_tag(url, :alt => alt, :width => width, :height => height)
+  end
+
 end
