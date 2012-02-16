9423533be93bc5dbdb47f3b00707454e52b0985c
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-02-16 12:56:29 +0700

    Отображение ошибок

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 module ApplicationHelper
-
   def news_image(url, alt = '')
     width, height = url.match(%r{files/\d+/(\d+)-(\d+)})[1..2]
     image_tag(url, :alt => alt, :width => width, :height => height)
   end
 
+  def render_partial_for_region(region)
+    if region && region.response_status == 200
+      render :partial => "regions/#{region.type.underscore}",
+             :locals => { :object => region.content, :response_status => region.response_status }
+    else
+      render :partial => 'regions/error', :locals => { :region => region }
+    end
+  end
 end
