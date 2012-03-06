759a199328f3654bb09e279305c1d1d657c9fe09
Evgeny Lapin <lev@openteam.ru>
2012-03-06 12:04:27 +0700

    show poll result inside

---
+++ app/controllers/poll_controller.rb
@@ -0,0 +1,14 @@
+class PollController < ApplicationController
+
+  def show
+    if params[:code]
+      uri = URI("http://webanketa.com/forms/#{params[:code]}/ru/statistic/")
+      result = Net::HTTP.get_response(uri)
+      if result.is_a?(Net::HTTPSuccess) && !result.body.match('Unfortunately, this form does not exist')
+        render :text => result.body.gsub('href="', 'href="http://webanketa.com').gsub('src="', 'src="http://webanketa.com'), :layout => false and return
+      end
+    end
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Inotomsk::Application.routes.draw do
+  match "poll" => "poll#show", :via => :get
   get '/(*path)', :to => 'main#index'
+  root :to => 'main#index'
 end
