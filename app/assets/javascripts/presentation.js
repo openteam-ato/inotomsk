518a98010b869a5bc553949f4d6b7bd71680b8f6
Evgeny Lapin <lev@openteam.ru>
2012-03-30 12:09:03 +0700

    about presentation: stub main menu toggle

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -1,2 +1,18 @@
+function trigger_main_items() {
+  $('.presentation .accordion > li > p > a').click(function() {
+    var link = $(this),
+        clicked_item = link.closest('li'),
+        selected_item = $('.selected', link.closest('.accordion'));
+    if (!clicked_item.hasClass('selected')) {
+      $('ul', selected_item).slideUp('fast');
+      selected_item.removeClass('selected');
+      $('ul', clicked_item).slideDown('fast');
+      clicked_item.addClass('selected');
+    };
+    return false;
+  });
+};
+
 $(function() {
+  trigger_main_items();
 });
