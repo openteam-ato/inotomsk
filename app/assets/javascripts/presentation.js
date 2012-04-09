4981b21edd21c0215398a53844b3df11546ff9b8
Evgeny Lapin <lev@openteam.ru>
2012-04-09 16:43:26 +0700

    accordion like menu in about page

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -1,13 +1,13 @@
 function trigger_main_items() {
-  $('.presentation .accordion > li > p > a').click(function() {
+  $('.accordion > li > p > a').click(function() {
     var link = $(this),
         clicked_item = link.closest('li'),
         selected_item = $('.selected', link.closest('.accordion'));
     if (!clicked_item.hasClass('selected')) {
-      $('ul', selected_item).slideUp('fast');
-      selected_item.removeClass('selected');
-      $('ul', clicked_item).slideDown('fast');
+      $('ul', clicked_item).slideDown();
       clicked_item.addClass('selected');
+      $('ul', selected_item).slideUp();
+      selected_item.removeClass('selected');
     };
     return false;
   });
