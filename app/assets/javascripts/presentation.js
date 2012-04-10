4806e33ba29088579c4697f275a3b37ca94f2cd6
Evgeny Lapin <lev@openteam.ru>
2012-04-10 15:04:55 +0700

    about project: update js for presentation

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -10,15 +10,29 @@ function trigger_main_items() {
       $('ul', selected_item).slideUp();
       selected_item.removeClass('selected');
       if (visible_submenu.length) {
-        var first_submenu = $('li:first', visible_submenu)
+        var first_submenu = $('li:first', visible_submenu);
         first_submenu.addClass('selected');
-        $('.presentation .content div').hide();
-        console.log($('a', first_submenu).attr('href'));
-        $('.presentation .content ' + $('a', first_submenu).attr('href')).show();
+        if ($('.presentation .content ' + $('a', first_submenu).attr('href')).length) {
+          $('.presentation .content div:visible').fadeOut(300, function() {
+            $('.presentation .content ' + $('a', first_submenu).attr('href')).fadeIn(300);
+          });
+        };
       } else {
-        $('.presentation .content div').hide();
-        $('.presentation .content ' + $(this).attr('href')).show();
+        $('.presentation .content div:visible').fadeOut(300, function() {
+          $('.presentation .content ' + link.attr('href')).fadeIn(300);
+        });
       };
+    };
+    return false;
+  });
+  $('.presentation .accordion > li > ul > li > a').click(function() {
+    var link = $(this);
+    if (!link.parent().hasClass('selected') && $('.presentation .content ' + link.attr('href')).length) {
+      link.parent().siblings().removeClass('selected');
+      link.parent().addClass('selected');
+      $('.presentation .content div:visible').fadeOut(300, function() {
+        $('.presentation .content ' + link.attr('href')).fadeIn(300);
+      });
     };
     return false;
   });
