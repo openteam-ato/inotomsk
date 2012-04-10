db91d96a6ac16d1842f00525be473a8097b76ec4
Evgeny Lapin <lev@openteam.ru>
2012-04-10 16:16:57 +0700

    about projects: first sub content, fix js

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -1,4 +1,5 @@
 function trigger_main_items() {
+  var speed_animation = 200;
   $('.presentation .accordion > li > p > a').click(function() {
     var link = $(this),
         clicked_item = link.closest('li'),
@@ -13,13 +14,13 @@ function trigger_main_items() {
         var first_submenu = $('li:first', visible_submenu);
         first_submenu.addClass('selected');
         if ($('.presentation .content ' + $('a', first_submenu).attr('href')).length) {
-          $('.presentation .content div:visible').fadeOut(300, function() {
-            $('.presentation .content ' + $('a', first_submenu).attr('href')).fadeIn(300);
+          $('.presentation .content div:visible').fadeOut(speed_animation, function() {
+            $('.presentation .content ' + $('a', first_submenu).attr('href')).fadeIn(speed_animation);
           });
         };
       } else {
-        $('.presentation .content div:visible').fadeOut(300, function() {
-          $('.presentation .content ' + link.attr('href')).fadeIn(300);
+        $('.presentation .content div:visible').fadeOut(speed_animation, function() {
+          $('.presentation .content ' + link.attr('href')).fadeIn(speed_animation);
         });
       };
     };
@@ -30,8 +31,8 @@ function trigger_main_items() {
     if (!link.parent().hasClass('selected') && $('.presentation .content ' + link.attr('href')).length) {
       link.parent().siblings().removeClass('selected');
       link.parent().addClass('selected');
-      $('.presentation .content div:visible').fadeOut(300, function() {
-        $('.presentation .content ' + link.attr('href')).fadeIn(300);
+      $('.presentation .content div:visible').fadeOut(speed_animation, function() {
+        $('.presentation .content ' + link.attr('href')).fadeIn(speed_animation);
       });
     };
     return false;
