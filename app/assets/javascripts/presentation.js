f3518bb5921682f44b39b19374379aa643b568dc
Evgeny Lapin <lev@openteam.ru>
2012-04-10 13:00:04 +0700

    about project: trigger on change main menu items

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -1,13 +1,24 @@
 function trigger_main_items() {
-  $('.accordion > li > p > a').click(function() {
+  $('.presentation .accordion > li > p > a').click(function() {
     var link = $(this),
         clicked_item = link.closest('li'),
         selected_item = $('.selected', link.closest('.accordion'));
     if (!clicked_item.hasClass('selected')) {
+      var visible_submenu = $('ul', clicked_item);
       $('ul', clicked_item).slideDown();
       clicked_item.addClass('selected');
       $('ul', selected_item).slideUp();
       selected_item.removeClass('selected');
+      if (visible_submenu.length) {
+        var first_submenu = $('li:first', visible_submenu)
+        first_submenu.addClass('selected');
+        $('.presentation .content div').hide();
+        console.log($('a', first_submenu).attr('href'));
+        $('.presentation .content ' + $('a', first_submenu).attr('href')).show();
+      } else {
+        $('.presentation .content div').hide();
+        $('.presentation .content ' + $(this).attr('href')).show();
+      };
     };
     return false;
   });
