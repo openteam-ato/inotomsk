cbd19c8e658ebba159540c7e346d3eea4c9ef585
Evgeny Lapin <lev@openteam.ru>
2015-07-16 10:57:23 +0600

    resolve sso ability

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -1,6 +1,7 @@
 class Manage::ApplicationController < ApplicationController
-  sso_authenticate_and_authorize
-  load_and_authorize_resource
+
+  sso_load_and_authorize_resource
 
   layout 'manage'
+
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,11 @@ class Ability
   def initialize(user)
     return unless user
 
+    can :manage, :application do
+      user.permissions.any?
+    end
+
     can :manage, Permission if user.manager?
+
   end
 end
