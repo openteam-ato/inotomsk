c000e98b5820185099dd73cb62ef896f512fc5fb
PeoneEr <peoneerko@yandex.ru>
2015-08-12 16:26:38 +0600

    fix http://errors.openteam.ru/apps/b79a9aba5b9fe13038222da3747e1c8a/problems/55cb1e7407213a07a22296e6

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -1,5 +1,5 @@
 class DirectionsController < MainController
   def show
-    @placemarks = MapLayer.find(params[:slug]).placemarks
+    @placemarks = MapLayer.find(params[:slug]).children.flat_map(&:placemarks)
   end
 end
