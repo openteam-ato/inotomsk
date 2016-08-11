807ed0df7336198ff44d3d0c77f42d7beaa47213
PeoneEr <peoneerko@yandex.ru>
2016-08-11 16:38:12 +0700

    Associate map layers with user

--- app/controllers/workplace/user_tags_controller.rb
+++ app/controllers/workplace/user_tags_controller.rb
@@ -10,9 +10,14 @@ module Workplace
 
     def update
       @user.user_tags.where(tag: @user.user_tags.pluck(:tag) - params[:user][:tags]).map(&:destroy)
+      @user.user_map_layers.where(map_layer: @user.user_map_layers.pluck(:map_layer) - params[:user][:user_map_layers]).map(&:destroy)
 
       params[:user][:tags].each do |tag|
         @user.user_tags.create tag: tag
+      end
+
+      params[:user][:user_map_layers].each do |map_layer|
+        @user.user_map_layers.create map_layer: map_layer
       end
 
       redirect_to workplace_user_tag_path(@user)
