ec317def83b0b375b8dbfd7e04753828d3354484
Evgeny Lapin <lev@openteam.ru>
2016-12-20 11:57:52 +0700

    updated some configs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w[
   ie_6.css
   ie_lower_8.css
   stop_ie.css
-)
+]
